1841990cfa1191249816866bc5358f7b3e367060
Bagdatr <bagdat.rakhimov@gmail.com>
2023-04-04 16:30:08 +0600

    SprintTask_3_1 re-finished

---
+++ JavaSpring/springSprintTask_3_1/src/main/java/kz/bitlab/springSprintTask_3_1/SpringSprintTask31Application.java
@@ -0,0 +1,13 @@
+package kz.bitlab.springSprintTask_3_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSprintTask31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSprintTask31Application.class, args);
+	}
+
+}
---
+++ JavaSpring/springSprintTask_3_1/src/test/java/kz/bitlab/springSprintTask_3_1/SpringSprintTask31ApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.springSprintTask_3_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSprintTask31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
